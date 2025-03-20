eda73f559d12f7a92e07fab81a4bf92e26e5ed07
Paulo Henrique Raiol <pauloraiol@hotmail.com>
2025-03-19 21:25:01 -0300

    trabalhando com flatlist e outros

--- imHere/src/screens/Home/index.tsx
+++ imHere/src/screens/Home/index.tsx
@@ -1,19 +1,46 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, FlatList } from "react-native";
 import { styles } from "./styles";
-import { obterDataAtualFormatada } from '../../Utils'
+import { obterDataAtualFormatada } from "../../utils";
+import { Participant } from "../../components/Participant";
 
 export default function Home() {
+  const participants = ['Paulo', 'João', 'Maria', 'José', 'Ana', 'Pedro', 'Julia', 'Antonio', 'Carlos', 'Fernanda', 'Augusto', 'Bruno', 'Breno'];
+
+  function handleParticipantAdd(){
+    console.log("Você clicou no botão de adicionar!");
+  }
+
+  function handleParticipantRemove(name: string){
+    console.log(`Você clicou em remover o participante ${name}!`);
+  }
+
   return(
     <View style={styles.container}>
       <View style={styles.headerEvent}>
         <Text style={styles.eventName}>Nome do Evento</Text>
         <Text style={styles.eventDate}>{obterDataAtualFormatada()}</Text>
       </View>
-      <TextInput 
-        style={styles.input}
-        placeholder='Nome do participante'
-        placeholderTextColor='#6B6B6B'
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={ item => item}
+        renderItem={ ({item}) => <Participant key={item} name={item} onRemove={() => handleParticipantRemove(item)} />}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>Nenhum participante cadastrado.</Text>
+        )}
       />
+
     </View>
   );
 }
--- imHere/src/screens/Home/styles.ts
+++ imHere/src/screens/Home/styles.ts
@@ -4,10 +4,10 @@ export const styles = StyleSheet.create({
     container:{
       flex: 1,
       backgroundColor: '#131016',
+      paddingHorizontal: 24,
     },
     headerEvent:{
       marginTop: 50,
-      marginLeft: 24,
     },
     eventName:{
       color: '#FDFCFE',
@@ -18,13 +18,39 @@ export const styles = StyleSheet.create({
       color: '#6B6B6B'
     },
     input:{
+      flex: 1,
       backgroundColor: '#1F1E25',
       height: 56,
+      width: '70%',
       borderRadius: 5,
       color: '#FDFCFE',
       padding: 16,
       fontSize: 16,
       marginTop: 15,
-      marginHorizontal: 24,
+      marginRight: 12
     },
+    buttonText:{
+      color: '#FFF',
+      fontSize: 24,
+    },
+    button:{
+      width: 56,
+      height: 56,
+      borderRadius: 5,
+      backgroundColor: '#31CF67',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    form:{
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'flex-end',
+      marginTop: 20,
+      marginBottom: 22,
+    },
+    listEmptyText:{
+      color: '#FFF',
+      fontSize: 14,
+      textAlign: 'center',
+    }
   });
