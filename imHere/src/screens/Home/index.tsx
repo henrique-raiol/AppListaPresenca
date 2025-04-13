337e11277d280211d29181ba5aafc7ee74d94c5c
Paulo Henrique Raiol <pauloraiol@hotmail.com>
2025-04-12 21:00:23 -0300

    Conclusão do App

--- imHere/src/screens/Home/index.tsx
+++ imHere/src/screens/Home/index.tsx
@@ -1,17 +1,34 @@
-import { View, Text, TextInput, TouchableOpacity, FlatList } from "react-native";
-import { styles } from "./styles";
-import { obterDataAtualFormatada } from "../../utils";
-import { Participant } from "../../components/Participant";
+import { View, Text, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { styles } from './styles';
+import { obterDataAtualFormatada } from '../../utils';
+import { Participant } from '../../components/Participant';
 
 export default function Home() {
-  const participants = ['Paulo', 'João', 'Maria', 'José', 'Ana', 'Pedro', 'Julia', 'Antonio', 'Carlos', 'Fernanda', 'Augusto', 'Bruno', 'Breno'];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState<string>('');
 
   function handleParticipantAdd(){
-    console.log("Você clicou no botão de adicionar!");
+    if(participants.includes(participantName)){
+      return Alert.alert('Partcipante existe', 'Já existe um participante na lista com esse nome');
+    }
+    
+    setParticipants(prevSate => [...prevSate, participantName]);
+    setParticipantName('');
   }
 
   function handleParticipantRemove(name: string){
-    console.log(`Você clicou em remover o participante ${name}!`);
+
+    Alert.alert('Remover', `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => setParticipants(prevState => prevState.filter( participant => participant !== name))
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ]);
   }
 
   return(
@@ -23,8 +40,10 @@ export default function Home() {
       <View style={styles.form}>
         <TextInput 
           style={styles.input}
-          placeholder="Nome do participante"
-          placeholderTextColor="#6B6B6B"
+          placeholder='Nome do participante'
+          placeholderTextColor='#6B6B6B'
+          onChange={e => setParticipantName(e.nativeEvent.text)}
+          value={participantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
